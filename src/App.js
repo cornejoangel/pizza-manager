2093e8d7495732290c5d22867f8ea2a6effa5f31
Angel Cornejo <self.angelcornejo@gmail.com>
2022-10-19 11:56:06 -0700

    Adds basic structure, topping list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import ToppingMenu from "./ToppingMenu";
+import { useState } from "react";
+
 const App = () => {
-  const hello = <div>Hello World</div>;
-  return hello;
+  const [toppings] = useState(ToppingMenu());
+  const [toppingsMenu, setToppingsMenu] = useState(toppings.getMenu());
+
+  return (
+    <main>
+      <div>
+        <h2>Toppings</h2>
+        <ul>
+          {toppingsMenu.map((t) => (
+            <li key={t}>{t}</li>
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
 }
 
 export default App;
